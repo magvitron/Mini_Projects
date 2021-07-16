c0cdd2bdf02d5b0358fd9e8b671eedc1f29a446a
magvitron <magvitron@gmail.com>
2021-07-16 08:38:06 +0530

    added comments

--- Alpha_Numeric_keypad/Alpha_Numeric_keypad/Alpha_Numeric_keypad.ino
+++ Alpha_Numeric_keypad/Alpha_Numeric_keypad/Alpha_Numeric_keypad.ino
@@ -1,93 +1,137 @@
-#include <Keypad.h>
+/**
+   @file Alpha_Numeric_Keypad
+   @breif Program for old scholl mobile typing in generic
+   4 row 3 col number pad
+   @author Manu krishnan magvitron@gmail.com
+   @date 16-07-2021
+*/
 #define DEBUG 1
-#define ARRAY_SIZE 24
+#define ARRAY_SIZE 24 /**Global array size*/
+#define TIME_OUT 50 /**Time out for key press after this
+  the key will asign the value*/
+/*
+  @version 1.0
+  @author Alexander Brevig
+*/
+#include <Keypad.h>
 
-#define TIME_OUT 50
 enum KEYPAD_TYPE {
   ALPHA_KEYPAD = 0,
   UPPER_CASE_KEYPAD = 1,
   NUMERIC_KEYPAD = 2
 };
-
-const byte ROWS = 4; //four rows
-const byte COLS = 3; //three columns
-
+/**Keypad HW setup start*/
+const byte ROWS = 4; /*four rows*/
+const byte COLS = 3; /*three columns*/
+/**Alpha key pad*/
 char ALPHA_keys[ROWS][COLS] = {
   {' ', 'a', 'd'},
   {'g', 'j', 'm'},
   {'p', 't', 'w'},
   {'*', ' ', '#'}
 };
+/**Numeric*/
 char NUM_keys[ROWS][COLS] = {
   {'1', '2', '3'},
   {'4', '5', '6'},
   {'7', '8', '9'},
   {'*', '0', '#'}
 };
+byte rowPins[ROWS] = {11, 12, 13, A0}; /*connect to the row pinouts of the keypad*/
+byte colPins[COLS] = {10, 9, 8}; /*connect to the column pinouts of the keypad*/
+/**Keypad HW setup end*/
 
-byte rowPins[ROWS] = {11, 12, 13, A0}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {10, 9, 8}; //connect to the column pinouts of the keypad
 uint8_t keyPadMode = ALPHA_KEYPAD;
-
 uint8_t index = 0;
 char GAC_Data[ARRAY_SIZE] = {0};
-//Create an object of keypad
+/**Create an object of keypad*/
 Keypad Alphakeypad = Keypad( makeKeymap(ALPHA_keys), rowPins, colPins, ROWS, COLS );
 Keypad Numkeypad = Keypad( makeKeymap(NUM_keys), rowPins, colPins, ROWS, COLS );
+/**
+  @bried Setup (arduino function)
+  @param Nothing
+  @return Nothing
+*/
 void setup() {
   Serial.begin(9600);
   Serial.println("Started");
 }
 /*
-   get the typf of key
-
+  @brief Check the type of keypad and return the key
+  @param keypaMode as global (alpha , upper
+         or nmeric
+  @return char, the key value
 */
 
 char get_Type_Key()
 {
   char LC_key = 0;
+  /**Check for the key pad*/
   if (keyPadMode == ALPHA_KEYPAD)
   {
-    LC_key = Alphakeypad.getKey();// Read the key
+    /**Diffrent object for alpha*/
+    LC_key = Alphakeypad.getKey();/* Read the key*/
   }
   else if (keyPadMode == NUMERIC_KEYPAD)
   {
-    LC_key = Numkeypad.getKey();// Read the key
+    /**Diffrent object keypad for num key*/
+    LC_key = Numkeypad.getKey();/* Read the key*/
   }
   else if (keyPadMode == UPPER_CASE_KEYPAD)
   {
-    LC_key = (Alphakeypad.getKey()); // Read the key
+    LC_key = (Alphakeypad.getKey()); /** Read the key*/
+    /**check if the key pad value is * this is
+      required since we are offseting the key value and * is
+      a control key which changes the key pad
+    */
     if (LC_key && LC_key != '*')
     {
+      /**offset 32 points for making it as upper case (reffer ASCCI table)      */
       LC_key -= 32;
     }
   }
+  /***/
   return LC_key;
 }
-
+/**
+  @breif Main loop
+  @return Nothing
+  @param Nothing
+*/
 void loop() {
+  /**  Declare the variables  */
   uint8_t timeOutCount = 0;
   char key, tempKey, temp;
   uint8_t upCount = 2;
+  /**  Get the key  */
   key = get_Type_Key();
   if (key)
   {
+    /** check for control keys (* -> chages the keypad layoyt) */
     if (key == '*')
     {
+      /** Print the character array*/
       Serial.println(GAC_Data);
+      /** Change the keypad mode*/
       keyPadMode++;
+      /**Rest to ALPHA keypad prevent overflow*/
       if (keyPadMode > NUMERIC_KEYPAD)
       {
         keyPadMode = ALPHA_KEYPAD;
       }
-      delay(500);
+      /**Reset the key variable, since its expired*/
       key = 0;
+      /**Print the current mode*/
       Serial.print("Alpha=0, upper=1, numeric:2 Current :");
       Serial.println(keyPadMode);
+      delay(500);
     }
+    /**Control '#' for clearing the global array */
     else if (key == '#')
     {
+      /** Print the array in memory*/
       Serial.println(GAC_Data);
+      /**Clear the array */
       for (int count = 0; count < ARRAY_SIZE; count++)
       {
         GAC_Data[count] = 0;
@@ -96,31 +140,50 @@ void loop() {
     }
     else
     {
+      /** If its not a control key, its an alpha numeric key */
+
+      /** Save the key variabe*/
       tempKey = key;
       Serial.println(key);
+      /** Check for p,w since in key pad this is 4 times , eg
+        key pad 2 is abc but keypad 7 is pqrs so the increment count is diffrent
+      */
       if ((key == 'p' || key == 'w' || key == 'P' || key == 'W'))
       {
+        /** CHange the incremement count*/
         upCount = 3;
       }
       else if (key == ' ')
       {
+        /** Check for the space bar key (0 and 1) and reset the upcount*/
         upCount = 0;
       }
+      /**Check for numeric keypad, and we dont require increment here too */
       else if (keyPadMode == NUMERIC_KEYPAD)
       {
         upCount = 0;
       }
+      /**Check for time out,(the time out variable is set at the top
+        if the time out is expired, break and save the current key to the global array
+      */
       while (TIME_OUT >= timeOutCount)
       {
         temp = get_Type_Key();
         if (temp)
         {
+          /** Check if the user pressed the same key or not (if diffrent prevent the increment)*/
           if (temp != tempKey)
           {
+            /** User pressed diffent key*/
             timeOutCount = TIME_OUT;
           } else {
+            /** Incremnt key so here , the increment is done with ASCCI in mind, 'a' is 97 so
+                if we increment it will be 98 -> 'b' 99 -> 'c'
+            */
             key++;
             timeOutCount = 0;
+            /** Reset to the initial value id it exceeds the upcount, #pad7,9 -> 4 time
+              other keys 3 times */
             if (key - tempKey > upCount)
             {
               key = tempKey;
@@ -128,15 +191,20 @@ void loop() {
             Serial.println(key);
           }
         }
-
+        /**Delay for a small time 10ms */
         delay(10);
+        /** Increment timeout*/
         timeOutCount++;
       }
+      /** Save in gloabal array*/
       GAC_Data[index] = key;
       index++;
+      /** Reset the index of global array*/
       if (index > ARRAY_SIZE)
         index = 0;
+      /**PRint the array*/
       Serial.println(GAC_Data);
     }
   }
 }
+/** End of file!*/
